813c4132f4c9bb41a96e0a0d40144e47f79ee76a
蒋军 <513358386@qq.com>
2017-05-05 14:07:58 +0800

    post-card can show static comment with transition

--- src/views/components/PostCard.vue
+++ src/views/components/PostCard.vue
@@ -51,17 +51,65 @@
     <v-card-row actions :class="item.bottom_color != null ? item.bottom_color : 'white'">
       <v-btn flat :class="item.bottom_text_color != null ? item.bottom_text_color : 'grey--text text--darken-4'">点赞呐</v-btn>
       <v-spacer></v-spacer>
-      <v-btn icon>
+      <v-btn @click.native="showCommentValue = !showCommentValue" icon>
         <v-icon :class="item.bottom_text_color != null ? item.bottom_text_color : 'grey--text text--darken-4'">comment</v-icon>
       </v-btn>
     </v-card-row>
 
 
+    <transition name="slide-fade">
+      <v-list v-if="showCommentValue" three-line >
+        <template v-for="i in 30">
+
+          <v-list-item v-bind:key="'title'">
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img v-bind:src="'/static/head.png'"/>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title v-html="'<p>某位游客</p>'"/>
+                <v-list-tile-sub-title v-html="'<p>说得好</p>'"/>
+              </v-list-tile-content>
+
+              <v-list-tile-action>
+                <p style="font-weight:200">5小时</p>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list-item>
+        </template>
+      </v-list>
+
+
+    </transition>
+
+
+
   </v-card>
 </template>
+<style>
+
+/* 可以设置不同的进入和离开动画 */
+/* 设置持续时间和动画函数 */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-active {
+  transform: translateY(-50px);
+  opacity: 0;
+}
+
+</style>
 
 <script>
   export default {
+  data () {
+    return {
+      showCommentValue: false
+      }
+    },
     name: 'PostCard',
     props: {
       item: {
@@ -70,6 +118,9 @@
       }
     },
     methods:{
+      showComment(){
+        console.log('showComment')
+      },
       tagClicked(id, name){
         this.$emit('tagClicked', id, name)
       }
--- src/views/tourists/Post.vue
+++ src/views/tourists/Post.vue
@@ -1,41 +1,39 @@
 <template>
   <div>
-    
+
     <v-row >
+      <v-col xs1 sm1 lg1 xl1 style="float:left;">
+      </v-col >
 
-     <v-col xs1 sm1 lg1 xl1 style="float:left;">
-     </v-col >
-
-     <!-- 中间的一列 -->
-     <v-col xs10 sm10 lg10 xl10 style="float:left;">
+      <!-- 中间的一列 -->
+      <v-col xs10 sm10 lg10 xl10 style="float:left;">
          <post-card  :item="post" :imgHeight="'500px'"></post-card>
-     </v-col>
+      </v-col>
 
-     <v-col xs1 sm1 lg1 xl1 style="float:left;">
-     </v-col >
-     <!-- TODO: 点击左右切换 post -->
-    </v-row>
+      <v-col xs1 sm1 lg1 xl1 style="float:left;">
+      </v-col >
+      <!-- TODO: 点击左右切换 post -->
+     </v-row>
+
+     <v-card style="width:100%;box-sizing:''; margin-top:5px" >
+       <v-card-row actions style=" background: #e0e0e0;  ">
+         <v-btn v-if="earlier_post!=null" @click.native="getPosts(earlier_post.id)" flat :class="'grey--text text--darken-4'">{{earlier_post.title}}</v-btn>
+         <v-spacer></v-spacer>
+         <v-btn v-if="later_post!=null" @click.native="getPosts(later_post.id)" flat :class="'grey--text text--darken-4'">{{later_post.title}}</v-btn>
+       </v-card-row>
+     </v-card>
 
 
-    <v-card style="width:100%;box-sizing:''; margin-top:5px" >
-      <v-card-row actions style=" background: #e0e0e0;  ">
-        <v-btn v-if="earlier_post!=null" @click.native="getPosts(earlier_post.id)" flat :class="'grey--text text--darken-4'">{{earlier_post.title}}</v-btn>
-        <v-spacer></v-spacer>
-   <!--      <v-btn icon>
-          <v-icon :class="'grey--text text--darken-4'">keyboard_arrow_right</v-icon>
-        </v-btn> -->
-
-        <v-btn v-if="later_post!=null" @click.native="getPosts(later_post.id)" flat :class="'grey--text text--darken-4'">{{later_post.title}}</v-btn>
-
-      </v-card-row>
-    </v-card>
   </div>
 
 
    
 </template>
+<style>
 
+</style>
 <script>
+
 var marked = require('marked'); // 引入marked (markdown to html)
 
 import PostCard from '../components/PostCard'
@@ -65,7 +63,6 @@ export default {
     getPosts(id){
        this.axios.post('/api/get_posts_by_id', {id: id})    
        .then((response) => {   
-          console.log(response.data)
           this.initData(response.data)
        })    
        .catch(function (error) {   
